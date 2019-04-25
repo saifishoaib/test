2c9cce4044ed2af04a1b535f68d7936b38384dbd
shoaib <shoaibprimetime@gmail.com>
2019-04-25 15:29:40 +0530

    OOps.php file is added

---
+++ oops.php
@@ -0,0 +1,34 @@
+<body>Loremp Imsum
+<h1>Loremp Imsum</h2>
+</body>
+<?php
+class Foo
+{
+	
+
+	
+	public function test()
+	{
+	
+	echo 'this is Parent Class method';
+	}
+	
+}
+Class F extends Foo{
+	public function test(){
+		echo 'this is Child Class Method';
+	}
+}
+
+
+$obj = new F;
+$obj->test();
+
+
+
+var_dump(Foo::$foo);
+change in tghis in Hindi Titie
+
+
+
+?>
